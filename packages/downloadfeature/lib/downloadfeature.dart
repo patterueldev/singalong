a36f06f724682decf2e959602128881c09b81197
John Patrick Teruel <jpteruel95@gmail.com>
2024-09-18 21:57:03 +0800

    feat: Adding views for download feature

--- packages/downloadfeature/lib/downloadfeature.dart
+++ packages/downloadfeature/lib/downloadfeature.dart
@@ -1,7 +1,5 @@
 library downloadfeature;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+part 'manual/songurlview.dart';
+part 'search/songsearchview.dart';
+part 'details/songdetailsview.dart';
