2582291c50267d9d5921158a9a7abd3ba7e61179
John Patrick Teruel <jpteruel95@gmail.com>
2024-10-11 18:26:51 +0800

    chore: Separate configurations for security

--- api/src/main/kotlin/io/patterueldev/singalong/AppConfiguration.kt
+++ api/src/main/kotlin/io/patterueldev/singalong/AppConfiguration.kt
@@ -7,65 +7,13 @@ import com.aallam.openai.client.OpenAIHost
 import com.aallam.openai.client.RetryStrategy
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
-import io.patterueldev.session.jwt.JwtAuthenticationEntryPoint
-import io.patterueldev.session.jwt.JwtAuthenticationProvider
-import io.patterueldev.session.jwt.JwtSecurityContextRepository
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.http.SessionCreationPolicy
-import org.springframework.security.web.SecurityFilterChain
 import org.springframework.web.reactive.function.client.WebClient
-import org.springframework.web.servlet.config.annotation.CorsRegistry
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-@EnableWebSecurity
 class AppConfiguration {
-    @Value("\${cors.allowedOrigins}")
-    private lateinit var allowedOrigins: String
-
-    @Autowired
-    private lateinit var jwtAuthenticationEntryPoint: JwtAuthenticationEntryPoint
-
-    @Autowired
-    private lateinit var jwtAuthenticationProvider: JwtAuthenticationProvider
-
-    @Autowired
-    private lateinit var jwtSecurityContextRepository: JwtSecurityContextRepository
-
-    @Bean
-    fun corsConfigurer(): WebMvcConfigurer {
-        return object : WebMvcConfigurer {
-            override fun addCorsMappings(registry: CorsRegistry) {
-                val origins = allowedOrigins.split(",").toTypedArray()
-                println("Allowed origins: ${origins.joinToString(" | ")}")
-                registry.addMapping("/**")
-                    .allowedOrigins(*origins)
-                    .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
-                    .allowedHeaders("*")
-                    .allowCredentials(true)
-            }
-        }
-    }
-
-    @Bean
-    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
-        return http.csrf { it.disable() }
-            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
-            .exceptionHandling { it.authenticationEntryPoint(jwtAuthenticationEntryPoint) }
-            .authenticationProvider(jwtAuthenticationProvider)
-            .securityContext { it.securityContextRepository(jwtSecurityContextRepository) }
-            .authorizeHttpRequests {
-                it.requestMatchers("/connect/**").permitAll()
-                it.anyRequest().authenticated()
-            }
-            .build()
-    }
-
     @Bean
     fun songDownloaderClient(
         @Value("\${baseurl.songdownloader}") songDownloaderUrl: String,
---
+++ api/src/main/kotlin/io/patterueldev/singalong/SecurityConfiguration.kt
@@ -0,0 +1,59 @@
+package io.patterueldev.singalong
+
+import io.patterueldev.session.jwt.JwtAuthenticationEntryPoint
+import io.patterueldev.session.jwt.JwtAuthenticationProvider
+import io.patterueldev.session.jwt.JwtSecurityContextRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfiguration {
+    @Autowired
+    private lateinit var jwtAuthenticationEntryPoint: JwtAuthenticationEntryPoint
+
+    @Autowired
+    private lateinit var jwtAuthenticationProvider: JwtAuthenticationProvider
+
+    @Autowired
+    private lateinit var jwtSecurityContextRepository: JwtSecurityContextRepository
+
+    @Bean
+    fun corsConfigurer(
+        @Value("\${cors.allowedOrigins}") allowedOrigins: String,
+    ): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                val origins = allowedOrigins.split(",").toTypedArray()
+                println("Allowed origins: ${origins.joinToString(" | ")}")
+                registry.addMapping("/**")
+                    .allowedOrigins(*origins)
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                    .allowedHeaders("*")
+                    .allowCredentials(true)
+            }
+        }
+    }
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        return http.csrf { it.disable() }
+            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+            .exceptionHandling { it.authenticationEntryPoint(jwtAuthenticationEntryPoint) }
+            .authenticationProvider(jwtAuthenticationProvider)
+            .securityContext { it.securityContextRepository(jwtSecurityContextRepository) }
+            .authorizeHttpRequests {
+                it.requestMatchers("/connect/**").permitAll()
+                it.anyRequest().authenticated()
+            }
+            .build()
+    }
+}
