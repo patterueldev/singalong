b87dbcdbbc3f061455bbf2f3258e1dba3115239c
John Patrick Teruel <jpteruel95@gmail.com>
2024-11-03 12:04:27 +0800

    feat: Renamed admin project

--- client/apps/controller_web/android/app/src/main/kotlin/com/example/controller_web/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.controller_web
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- client/apps/player_all/android/app/src/main/kotlin/com/example/player_all/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.player_all
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
