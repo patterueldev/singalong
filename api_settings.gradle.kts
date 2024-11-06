b2ec52c6dc97cbcd3a92b05b0cff5ed13c363047
John Patrick Teruel <jpteruel95@gmail.com>
2024-11-07 00:48:09 +0800

    chore: Segragate settings for includes

---
+++ api_settings.gradle.kts
@@ -0,0 +1,32 @@
+include(":common")
+project(":common").projectDir = file("./libs/common")
+
+include(":commonds")
+project(":commonds").projectDir = file("./libs/commonds")
+
+include(":mongo-ds")
+project(":mongo-ds").projectDir = file("./libs/mongo-ds")
+
+include(":session")
+project(":session").projectDir = file("./libs/session")
+
+include(":sessionds")
+project(":sessionds").projectDir = file("./libs/sessionds")
+
+include(":songidentifier")
+project(":songidentifier").projectDir = file("./libs/songidentifier")
+
+include(":songidentifierds")
+project(":songidentifierds").projectDir = file("./libs/songidentifierds")
+
+include(":songbook")
+project(":songbook").projectDir = file("./libs/songbook")
+
+include(":songbookds")
+project(":songbookds").projectDir = file("./libs/songbookds")
+
+include(":reservation")
+project(":reservation").projectDir = file("./libs/reservation")
+
+include(":reservationds")
+project(":reservationds").projectDir = file("./libs/reservationds")
---
+++ client_settings.gradle.kts
@@ -0,0 +1,8 @@
+include("admin-app")
+project(":admin-app").projectDir = file("apps/admin")
+
+include("controller-app")
+project(":controller-app").projectDir = file("apps/controller")
+
+include("player-app")
+project(":player-app").projectDir = file("apps/player")
